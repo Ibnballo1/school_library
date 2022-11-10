eb735c86433a910455afb47baebc3f990c79bd8e
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-10 22:26:13 +0100

    Add person class

---
+++ person.rb
@@ -0,0 +1,27 @@
+class Person
+  # attribute accessor for getters and setters
+  attr_accessor :id, :name, :age
+  # constructor
+  def initialize(name = "unknown", age, parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name= name
+    @age = age
+  end
+  # private method
+  private
+  def is_of_age
+    if age >= 18
+      return true
+    else
+      return false
+    end
+  end
+
+  # public method
+  def can_use_services
+    if age || parent_permission
+      return true
+    end
+  end
+
+end
