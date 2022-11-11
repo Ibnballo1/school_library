ce56bd2fb6718dbca5d7de9f3a390f087755c2bd
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-11 09:25:55 +0100

    Fix reuire changes

--- person.rb
+++ person.rb
@@ -3,21 +3,22 @@ class Person
   attr_accessor :id, :name, :age
 
   # constructor
-  def initialize(age, name = 'unknown', parent_permission: true) # rubocop:disable Lint/UnusedMethodArgument
+  def initialize(age, name = 'unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
   # private method
 
   private
 
   def of_age?
-    return true if age >= 18
+    return true if @age >= 18
   end
 
   # public method
   def can_use_services?
-    return unless age || parent_permission
+    return unless @age || @parent_permission
   end
 end
