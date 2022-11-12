4cff756c863cad56fdb616499efb23afd0e9ddab
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-12 13:16:42 +0100

    Add classroom class

---
+++ classroom.rb
@@ -0,0 +1,12 @@
+class classroom
+  attr_accessor :label
+  attr_reader :students
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student)
+  end
+end
