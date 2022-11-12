5130cad87ccfb89a07bf23b969b921665cd2f15b
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-12 19:22:05 +0100

    Fix errors

--- book.rb
+++ book.rb
@@ -11,4 +11,5 @@ class Book
   def add_rental(rental)
     @rentals.push(rental)
     rental.book = self
+  end
 end
--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,7 @@
-class classroom
+class Classroom
   attr_accessor :label
   attr_reader :students
+
   def initialize(label)
     @label = label
     @students = []
