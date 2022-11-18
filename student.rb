522c41ea1432585138d8400d7aef43de79a8caa8
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-18 07:43:15 +0100

    Update student file

--- student.rb
+++ student.rb
@@ -1,11 +1,20 @@
 require './person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_reader :classroom, :parent_permission
 
-  def initialize(classroom)
-    super()
+  def initialize(*all, classroom)
+    super(*all)
     @classroom = classroom
+  end
+
+  def self.create_student(age, name)
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp
+    parent_permission = parent_permission.downcase == 'y'
+    print 'Please enter a classroom: '
+    classroom = gets.chomp
+    new(age, name, parent_permission, classroom)
   end
 
   def classroom=(classroom)
