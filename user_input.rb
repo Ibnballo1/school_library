d5bb89f35871402d708b0e8a35b41221b65e0ecc
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-18 19:38:28 +0100

    Fix linters

--- user_input.rb
+++ user_input.rb
@@ -5,24 +5,24 @@ class UserInput
     people_json = []
     people.each do |person|
       person = if person.class.to_s == 'Teacher'
-                  {
-                    id: person.id.to_s,
-                    class: person.class.to_s,
-                    name: person.name,
-                    age: person.age,
-                    specialization: person.specialization,
-                    parent_permission: person.parent_permission.to_s
-                  }
-                else
-                  {
-                    id: person.id.to_s,
-                    class: person.class.to_s,
-                    name: person.name,
-                    age: person.age,
-                    classroom: person.classroom,
-                    parent_permission: person.parent_permission.to_s
-                  }
-                end
+                 {
+                   id: person.id.to_s,
+                   class: person.class.to_s,
+                   name: person.name,
+                   age: person.age,
+                   specialization: person.specialization,
+                   parent_permission: person.parent_permission.to_s
+                 }
+               else
+                 {
+                   id: person.id.to_s,
+                   class: person.class.to_s,
+                   name: person.name,
+                   age: person.age,
+                   classroom: person.classroom,
+                   parent_permission: person.parent_permission.to_s
+                 }
+               end
       people_json << person
       File.write('./people.json', JSON.pretty_generate(people_json, { indent: "\t", object_nl: "\n" }))
     end
