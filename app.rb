4b31fa536261cc68e17ddadceaa2c1d8f6ab58b4
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-18 17:11:40 +0100

    Update app

--- app.rb
+++ app.rb
@@ -1,19 +1,36 @@
-require './book'
-require './teacher'
-require './student'
-require './rental'
-require './person'
+require_relative './entities/book'
+require_relative './entities/teacher'
+require_relative './entities/student'
+require_relative './operations/rental'
+require_relative './entities/person'
 require 'json'
-require './user_input'
-require './user_output'
+require_relative './data/user_input'
+require_relative './data/user_output'
 
 class App
   def initialize()
-    puts "I am being created in the main.rb file"
+    @books = []
+    @people = []
+    @rentals = []
   end
 
-  def list_books(*books)
-    all_book = Book.new(*books)
-    all_book.*books
+  def run
+    UserOutput.load_people(@people)
+    UserOutput.load_books(@books)
+    UserOutput.load_rentals(@rentals)
+    user_input = 0
+    puts "####\nWelcome to the School Library Ruby App!\n####"
+    while user_input != '7'
+      puts "Choose a number from the options below:\n"
+      app_options
+      print 'Enter Option [number]: '
+      user_input = gets.chomp
+      puts "\n\n"
+      check_selection(user_input)
+    end
+    puts "Exiting, thanks for using this app!\n\n" if user_input == '7'
+    UserInput.save_people(@people)
+    UserInput.save_books(@books)
+    UserInput.save_rentals(@rentals)
   end
 end
