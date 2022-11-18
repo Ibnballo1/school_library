c09c6e15e4a5a131b293cbadd306e0513298339d
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-18 17:12:00 +0100

    Update main file

--- main.rb
+++ main.rb
@@ -1,53 +1,44 @@
-require './app.rb'
+require_relative 'app'
 
-def options()
-  lists = [
-    "List all books", "List all people", "Create a person", "Create a book",
-    "Create a rental", "List all rentals for a given person id", "Exit"
-  ]
-  1..7.times do |index|
-    puts "#{index + 1} - #{lists[index]}"
+def app_options
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+  puts "\n"
+end
+
+# rubocop:disable Metrics/CyclomaticComplexity
+def check_selection(response)
+  case response
+  when '1'
+    Book.list_all_books(@books)
+  when '2'
+    Person.list_all_people(@people)
+  when '3'
+    person = Person.create_person
+    @people << person
+    puts "#{person.name} created successfully" if @people.include?(person)
+  when '4'
+    book = Book.create_book
+    @books << book
+    puts "Book '#{book.title}' by #{book.author} created successfully" if @books.include?(book)
+  when '5'
+    rental = Rental.create_rental(@books, @people)
+    @rentals << rental
+    puts 'Rental created successfully' if @rentals.include?(rental)
+  when '6'
+    Rental.list_all_rentals_person_id(@people, @rentals)
   end
 end
+# rubocop:enable Metrics/CyclomaticComplexity
 
-def what_to_choose
-  your_choice = gets.chomp.to_i
-  while your_choice != 7
-    # what_to_choose()
-    case your_choice
-    when 1
-      puts "You choose 1"
-    when 2
-      puts "You choose 2"
-    when 3
-      print "Do you want to create a student (1) or a teacher (2)? [input the number]: "
-      choice_3 = gets.chomp.to_i
-      if choice_3 == 1
-        puts "You created a student"
-      else
-        puts "You created teacher"
-      end
-    when 4
-      puts "You choose 4"
-    when 5
-      puts "You choose 5"
-    when 6
-      puts "You choose 6"
-    else
-      puts "Your choose a wrong number"  
-    end
-    options()
-    your_choice = gets.chomp.to_i
-  end
-  puts "App exitted"
+def main
+  app = App.new
+  app.run
 end
 
-def main()
-  puts "Welcome to School Library App!"
-  puts "Please choose an option by entering a number"
-  puts ""
-  options()
-  what_to_choose()
-end
-
-main()
+main
