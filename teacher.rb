4c313c34e40cdf67b46e24563d9675a8b7f7b573
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-18 07:48:48 +0100

    Update teacher

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,17 @@
 require './person'
 
 class Teacher < Person
+  attr_accessor :specialization
   # constructor
-  def initialize(specialization)
-    super()
+  def initialize(*all, specialization)
+    super(*all)
     @specialization = specialization
+  end
+
+  def self.create_teacher(age, name)
+    print 'Specialization: '
+    specialization = gets.chomp
+    new(age, name, true, specialization)
   end
 
   def can_use_services
