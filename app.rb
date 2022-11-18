4fb15884e9f67bfc4cadb29b4e762f1fc3678ee2
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-18 08:24:19 +0100

    Add required files to app

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
+require './book'
+require './teacher'
+require './student'
+require './rental'
+require './person'
+require 'json'
+require './user_input'
+require './user_output'
+
 class App
   def initialize()
     puts "I am being created in the main.rb file"
+  end
+
+  def list_books(*books)
+    all_book = Book.new(*books)
+    all_book.*books
   end
 end
